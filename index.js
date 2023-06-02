045d4f49d72131600967e846ebae8a407a390d45
Mahadevan Shettiyar <79627324+sairaman705@users.noreply.github.com>
2023-06-02 11:39:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-ECHO is on.
+const mobile_nav = document.querySelector(".mobile-nav-btn");
+
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () =>{
+    nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener('click', () => toggleNavbar())
